67c7381c4aac8106095c9d999b3b29b0a7c452aa
Mutiara Noor Fauzia <143862669+moetzi@users.noreply.github.com>
2024-12-15 22:30:02 +0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  base: "/",
+
+  // Proxy configuration for API requests
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://portal-lomba-be.azurewebsites.net', // Your backend URL
+        changeOrigin: true,  // Allows for proxying API requests
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api prefix before forwarding to the backend
+      },
+    },
+  },
+
   plugins: [react()],
-})
+});
